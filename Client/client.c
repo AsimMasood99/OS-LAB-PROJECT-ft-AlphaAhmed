6959f1d29635de6fc3c1ec9d1a3f3efb5bcb1d57
asimmasood99 <macbookpro2089@gmail.com>
2024-08-31 12:59:41 +0500

    made parser

--- Client/client.c
+++ Client/client.c
@@ -5,15 +5,18 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "parser.h"
 
-int main() {
+int main()
+{
     int client_socket;
     struct sockaddr_in server_addr;
     char buffer[1024];
 
     // Create a socket
     client_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_socket < 0) {
+    if (client_socket < 0)
+    {
         perror("Error creating socket");
         exit(EXIT_FAILURE);
     }
@@ -24,25 +27,39 @@ int main() {
     server_addr.sin_port = htons(8080);
 
     // Connect to the server
-    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("Error connecting to server");
         exit(EXIT_FAILURE);
     }
 
-    // Send data to the server
-    const char *message = "Hello from client!";
-    send(client_socket, message, strlen(message), 0);
+    while (1)
+    {
+        
+        // Send data to the server
+        char command[256]; 
+        printf("Enter Command: "); 
+        fgets(command, sizeof(command), stdin);
+        printf("\nThe command is: %s\n", command);
+        char* parsed;
+        PARSER(command,parsed);
+        printf("responce from paser: %s",parsed);
+        
+        const char *message = "Hello from client!";
+        
+        send(client_socket, message, strlen(message), 0);
 
-    // Receive data from the server
-    memset(buffer, 0, sizeof(buffer));
-    int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
-    if (bytes_received < 0) {
-        perror("Error receiving data");
-        exit(EXIT_FAILURE);
+        // Receive data from the server
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_received = recv(client_socket, buffer, sizeof(buffer), 0);
+        if (bytes_received < 0)
+        {
+            perror("Error receiving data");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Received from server: %s\n", buffer);
     }
-
-    printf("Received from server: %s\n", buffer);
-
     close(client_socket);
     return 0;
 }
---
+++ Client/parser.h
@@ -0,0 +1,50 @@
+#ifndef PARSER_H
+#define PARSER_H
+#include <string.h>
+#include <stdio.h>
+
+// $upload$filePath$
+
+int isValidPath(const char* filePath){
+    FILE *file = fopen(filePath, "r");
+    if(file)
+        return 1;
+    return 0;
+
+
+}
+
+void PARSER(const char* command, char* parsedCommand){
+    char tempStr[256];
+    strcpy(tempStr, command);
+    tempStr[sizeof(tempStr)-1] = '\0';
+    char* commandType = strtok(tempStr, "$");
+    if(strcmp(commandType, "upload") == 0){
+        char* path = strtok(NULL,"$");
+        if(isValidPath(path)){
+            sprintf(parsedCommand, "{\"command\": \"%s\", \"path\": \"%s\"}", commandType, path);
+            return;
+        }
+        else 
+            strcpy(parsedCommand, "\"error\":\"Invalid Path\"");
+            return;
+    }
+    else if(strcmp(commandType, "download") == 0){
+        char* path = strtok(NULL,"$");
+        if(isValidPath(path)){
+            sprintf(parsedCommand, "{\"command\": \"%s\", \"path\": \"%s\"}", commandType, path);
+            return;
+        }
+        else 
+            strcpy(parsedCommand, "\"error\":\"Invalid Path\"");
+            return;
+    }
+    else {
+        strcpy(parsedCommand, "\"error\":\"Invalid Command\"");
+    }
+    
+}
+
+
+
+#endif 
