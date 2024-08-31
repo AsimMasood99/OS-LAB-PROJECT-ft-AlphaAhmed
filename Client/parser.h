916a3307d89f21e6099018ef1249f4859ad8fd58
asimmasood99 <macbookpro2089@gmail.com>
2024-08-31 18:18:43 +0500

    file path error solved

--- Client/parser.h
+++ Client/parser.h
@@ -2,15 +2,19 @@
 #define PARSER_H
 #include <string.h>
 #include <stdio.h>
-
+#include<sys/stat.h>
 // $upload$filePath$
 
 int isValidPath(const char* filePath){
-    FILE *file = fopen(filePath, "r");
-    if(file)
-        return 1;
-    return 0;
-
+    // FILE *file = fopen(filePath, "r");
+    // if(file)
+    //     return 1;
+    // return 0;
+      struct stat buffer;
+    if (stat(filePath, &buffer) == 0) {
+        return S_ISREG(buffer.st_mode); // Check if it is a regular file
+    }
+    return 0; 
 
 }
 
@@ -19,14 +23,16 @@ void PARSER(const char* command, char* parsedCommand){
     strcpy(tempStr, command);
     tempStr[sizeof(tempStr)-1] = '\0';
     char* commandType = strtok(tempStr, "$");
+
     if(strcmp(commandType, "upload") == 0){
+    
         char* path = strtok(NULL,"$");
         if(isValidPath(path)){
             sprintf(parsedCommand, "{\"command\": \"%s\", \"path\": \"%s\"}", commandType, path);
             return;
         }
         else 
-            strcpy(parsedCommand, "\"error\":\"Invalid Path\"");
+            strcpy(parsedCommand, "{\"error\":\"Invalid Path\"}");
             return;
     }
     else if(strcmp(commandType, "download") == 0){
@@ -36,11 +42,11 @@ void PARSER(const char* command, char* parsedCommand){
             return;
         }
         else 
-            strcpy(parsedCommand, "\"error\":\"Invalid Path\"");
+            strcpy(parsedCommand, "{\"error\":\"Invalid Path\"}");
             return;
     }
     else {
-        strcpy(parsedCommand, "\"error\":\"Invalid Command\"");
+        strcpy(parsedCommand, "{\"error\":\"Invalid Command\"}");
     }
     
 }
