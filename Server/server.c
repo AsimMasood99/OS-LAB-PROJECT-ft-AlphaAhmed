1f8d6e8c9cc5b2cd3a7533e667962cfec4187555
asimmasood99 <macbookpro2089@gmail.com>
2024-09-18 14:05:43 +0500

    Cleaned up server code

--- Server/server.c
+++ Server/server.c
@@ -7,30 +7,112 @@
 #include <arpa/inet.h>
 #include <cjson/cJSON.h>
 #include <sys/stat.h>
-int isSpaceAvailable(const char *path, int maxSpace)
-{
-    return 1;
-}
 
-struct isRecievingFile
+struct Recieving_File
 {
     int receiving;
     char *filename;
     int fileSize;
 };
 
-struct isSendingFile
+int isSpaceAvailable(const char *path, int maxSpace)
 {
-    int isSending;
-    char *filename;
-};
+    return 1;
+}
 
-#define BUFFERSIZE = 1024;
+int getFileSize(char *path)
+{
+    struct stat fileInfo;
+    stat(path, &fileInfo);
+    return fileInfo.st_size;
+}
+
+void recieve_file(char *content, char *filename, int filesize, int *totalRecievedBytes, int bufferBytes)
+{
+    FILE *file = fopen(filename, "ab");
+    if (!file)
+    {
+        perror("Error in openinig file: ");
+    }
+    if (file && *totalRecievedBytes < filesize)
+    {
+        fwrite(content, 1, filesize - *totalRecievedBytes > 1023 ? 1023 : filesize - *totalRecievedBytes - 1, file);
+        *totalRecievedBytes += bufferBytes;
+        fclose(file);
+    }
+}
+
+void handel_upload(int socket, cJSON *command, struct Recieving_File *recievingStatus, char *ip)
+{
+    char folder_name[256] = "./Server/Storage/";
+    strcat(folder_name, ip);
+
+    cJSON *status = cJSON_GetObjectItem(command, "status");
+    if (status && strcmp(status->valuestring, "incoming") == 0)
+    {
+        recievingStatus->receiving = 1;
+        recievingStatus->filename = malloc(strlen(folder_name) + strlen(cJSON_GetObjectItem(command, "filename")->valuestring) + 2);
+        sprintf(recievingStatus->filename, "%s/%s", folder_name, cJSON_GetObjectItem(command, "filename")->valuestring);
+        recievingStatus->fileSize = atoi(cJSON_GetObjectItem(command, "filesize")->valuestring);
+        return;
+    }
+    struct stat folder = {0};
+    // if there is a folder
+    if (stat(folder_name, &folder) == -1)
+    {
+        mkdir(folder_name, 0700);
+    }
+    char *response = "{\"status\":\"ready\", \"command\":\"upload\"}";
+    send(socket, response, strlen(response), 0);
+    return;
+}
+
+void send_file(int socket, char *filepath)
+{
+    char stream[1024];
+    FILE *file = fopen(filepath, "rb");
+
+    while (fgets(stream, 1024, file) != NULL)
+    {
+        send(socket, stream, strlen(stream), 0);
+    }
+
+    fclose(file);
+    return;
+}
+
+void handel_download(int socket, cJSON *command, char *ip)
+{
+    cJSON *fileToCheck = cJSON_GetObjectItem(command, "filename"); // jahaz ider filename ana tha
+    char folder_and_file_name[1024];
+    snprintf(folder_and_file_name, sizeof(folder_and_file_name), "./Server/Storage/%s/%s", ip, fileToCheck->valuestring);
+
+    if (access(folder_and_file_name, F_OK) == 0)
+    {
+        char download_response[256];
+        sprintf(download_response, "{\"status\":\"fetch\",\"command\":\"download\",\"filename\":\"%s\"}", fileToCheck->valuestring);
+        send(socket, download_response, strlen(download_response), 0);
+
+        send_file(socket, folder_and_file_name);
+        char *completion_Msg = "{\"status\":\"success\"}";
+        printf("File uploaded Successfully\n");
+
+        send(socket, completion_Msg, strlen(completion_Msg), 0);
+
+        return;
+    }
+    else
+    {
+        printf("File %s is not available on server\n", fileToCheck->valuestring);
+        char *failed_responce = "{\"status\":\"failed\"}";
+        send(socket, failed_responce, strlen(failed_responce), 0);
+    }
+}
+
 int main()
 {
     int server_socket, new_socket, client_addr_len;
-    struct isRecievingFile receivingFile = {0, NULL,0};
-    struct isSendingFile SendingFile = {0, NULL};
+    struct Recieving_File receivingFile = {0, NULL, 0};
     struct sockaddr_in server_addr, client_addr;
     char buffer[1024];
 
@@ -81,14 +163,10 @@ int main()
         while (1)
         {
             memset(buffer, 0, sizeof(buffer));
-            int bytes_received=1;
-            // here is where we are receiving data from client (both command and files)
-            if(SendingFile.isSending==0)
-            {
-                bytes_received = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
-            }
+            int bytes_received = 1;
             
-            //printf("buffer %s\n",buffer);
+            bytes_received = recv(new_socket, buffer, sizeof(buffer) - 1, 0);
+            
             if (bytes_received <= 0)
             {
                 if (bytes_received == 0)
@@ -104,31 +182,13 @@ int main()
             }
 
             buffer[bytes_received] = '\0';
-            // if this check is on then the client is sending us file content and we are storing it
+
             if (receivingFile.receiving)
             {
                 printf("reviing: %s\n", buffer);
-                //:strstr() is a predefined function used for string matching
-                // buffer is the main string to be examined.
-                // {\"status\":\"success\"} is the sub-string to be searched in string.
-
-                // here we are closing the file..
-
-
+            
+                recieve_file(buffer, receivingFile.filename, receivingFile.fileSize, &file_bytes, bytes_received);
                 
-                FILE *file = fopen(receivingFile.filename, "ab");
-                if(!file)
-                {
-                    perror("Error in openinig file: ");
-                }
-                if (file && file_bytes<receivingFile.fileSize)
-                {
-                    // fwrite(buffer, 1, bytes_received, file);
-                    printf("\nbam: %i\n",receivingFile.fileSize-file_bytes>1024?1024:receivingFile.fileSize-file_bytes);
-                    fwrite(buffer,1,receivingFile.fileSize-file_bytes>1023?1023:receivingFile.fileSize-file_bytes-1,file);
-                    file_bytes+=bytes_received;
-                    fclose(file);
-                }
                 if (strstr(buffer, "{\"status\":\"success\"}"))
                 {
                     receivingFile.receiving = 0;
@@ -138,37 +198,6 @@ int main()
                     continue;
                 }
             } // this else if is for sending file to the client
-            else if(SendingFile.isSending==1)
-            {
-                printf("Sending file\n");
-                int bytesRead = 0;
-                char stream[1024];
-                //char send_filepath[256];
-                // file path on the server about to be sent to client
-                //sprintf(send_filepath,"./Server/Storage/%s/%s",inet_ntoa(client_addr.sin_addr),SendingFile.filename);
-                FILE *_file = fopen(SendingFile.filename, "rb");
-                if(_file!=NULL)
-                {
-                    printf("File downloading in progress\n");
-                    printf("File name: %s \n",SendingFile.filename);
-                    while(fgets(stream,1024,_file)!=NULL){
-                        printf("%s",stream);
-                        send(new_socket, stream, sizeof(stream), 0);
-                    }   
-                    // this will happen when all files will be send    
-                    char* completion_Msg = "{\"status\":\"success\"}";
-                    printf("File uploaded Successfully\n");
-                    SendingFile.isSending=0;
-                    send(new_socket, completion_Msg, strlen(completion_Msg), 0);
-                }
-                else
-                {
-                    SendingFile.isSending=0;
-                    printf("Code RED abort\n");
-                }
-                
-                
-            }   
             else
             {
                 cJSON *jsonCommand = cJSON_Parse(buffer);
@@ -179,80 +208,13 @@ int main()
                 }
 
                 cJSON *commandType = cJSON_GetObjectItem(jsonCommand, "command");
-                cJSON *status = cJSON_GetObjectItem(jsonCommand, "status"); 
-                // if command is upload then do this
                 if (commandType && strcmp(commandType->valuestring, "upload") == 0)
                 {
-                    //making folder for the client
-                    char folder_name[256] = "./Server/Storage/";
-                    // changes IP of client from int to string and then concatinates clients IP address number (string) with folder name.
-                    strcat(folder_name, inet_ntoa(client_addr.sin_addr));
-
-                    struct stat folder = {0};
-                    // if there is a folder
-                    if (stat(folder_name, &folder) == -1)
-                    {
-                        mkdir(folder_name, 0700);
-                    }
-                    // this status means that file is ready and is coming
-                    if (status && strcmp(status->valuestring, "incoming") == 0)
-                    {
-                        printf("%s\n",cJSON_Print(jsonCommand));
-                        receivingFile.receiving = 1;
-                        receivingFile.filename = malloc(strlen(folder_name) + strlen(cJSON_GetObjectItem(jsonCommand, "filename")->valuestring) + 2);
-                        sprintf(receivingFile.filename,"%s/%s",folder_name,cJSON_GetObjectItem(jsonCommand, "filename")->valuestring);
-                        receivingFile.fileSize = atoi(cJSON_GetObjectItem(jsonCommand, "filesize")->valuestring);
-                        printf("%i",receivingFile.fileSize);
-                        continue;
-                    }
-                    else
-                    {
-                        char *response = "{\"status\":\"ready\", \"command\":\"upload\"}";
-                        send(new_socket, response, strlen(response), 0);
-                    }
-                } // this else if is for download command 
-                else if(commandType && strcmp(commandType->valuestring, "download") == 0)
+                    handel_upload(new_socket, jsonCommand, &receivingFile, inet_ntoa(client_addr.sin_addr));
+                }
+                else if (commandType && strcmp(commandType->valuestring, "download") == 0)
                 {
-                    cJSON *fileToCheck= cJSON_GetObjectItem(jsonCommand,"filename"); // jahaz ider filename ana tha
-                    char folder_and_file_name[1024];
-
-                    // changes IP of client from int to string and then concatinates clients IP address number (string) with folder name.
-                    snprintf(folder_and_file_name, sizeof(folder_and_file_name), "./Server/Storage/%s", inet_ntoa(client_addr.sin_addr));
-                    
-                    
-                    // Appending the "client.txt" to the folder path
-                    if (fileToCheck != NULL) {
-                        strncat(folder_and_file_name, "/", sizeof(folder_and_file_name) - strlen(folder_and_file_name) - 1);
-                        strncat(folder_and_file_name, fileToCheck->valuestring, sizeof(folder_and_file_name) - strlen(folder_and_file_name) - 1);
-                    } else {
-                        printf("Error: fileToCheck->valuestring is NULL\n");
-                        char *failed_responce = "{\"status\":\"failed\"}";
-                        send(new_socket, failed_responce, strlen(failed_responce), 0);
-                        // Handle the error
-                    }                    
-                    //snprintf(folder_and_file_name, sizeof(folder_and_file_name), "%s/%s", folder_and_file_name, fileToCheck->valuestring);
-
-                    // Check if the file exists using access() with F_OK (File OK)
-                    //printf("Path: %s\n", folder_and_file_name);
-                    if(access(folder_and_file_name,F_OK)==0)
-                    {
-                        char download_response[256];  // Allocate enough space for the formatted string
-                        sprintf(download_response, "{\"status\":\"fetch\",\"command\":\"download\",\"filename\":\"%s\"}", fileToCheck->valuestring);
-                        SendingFile.filename=folder_and_file_name;
-                        SendingFile.isSending=1;
-                        // Now send the formatted response
-                        send(new_socket, download_response, strlen(download_response), 0);
-                        //memset(download_response, 0, sizeof(download_response));
-                        printf("Starting to send file\n");
-                        
-                    }
-                    else
-                    {
-                        printf("File %s is not available on server\n", fileToCheck->valuestring);
-                        char *failed_responce = "{\"status\":\"failed\"}";
-                        send(new_socket, failed_responce, strlen(failed_responce), 0);
-                    }
-
+                    handel_download(new_socket, jsonCommand, inet_ntoa(client_addr.sin_addr));
                 }
                 else if (commandType && strcmp(commandType->valuestring, "close") == 0)
                 {
